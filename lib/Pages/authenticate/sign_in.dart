22df516815c47f4b9bcec454ef8300cbacac94bc
amit9530 <amitbracha55@gmail.com>
2021-05-20 03:06:48 +0300

    not complete edit

--- lib/Pages/authenticate/sign_in.dart
+++ lib/Pages/authenticate/sign_in.dart
@@ -22,86 +22,87 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.blue[400],
-        appBar: AppBar(
-          title: Text('Sign In'),
-          backgroundColor: Colors.blue[800],
-          elevation: 0.0,
-          actions: <Widget>[
-            TextButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Sign Up'),
-            )
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 20.0,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.blue[400],
+      appBar: AppBar(
+        title: Text('Sign In'),
+        backgroundColor: Colors.blue[800],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            onPressed: () {
+              widget.toggleView();
+            },
+            icon: Icon(Icons.person),
+            label: Text('Sign Up'),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Email',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                TextFormField(
+                  validator: (val) =>
+                      val.isEmpty ? 'Please Enter email address' : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Password',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  validator: (val) => val.length < 8
+                      ? 'Password should contains at least 8 digit'
+                      : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.blue),
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
                   ),
-                  Text(
-                    'Email',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  TextFormField(
-                    validator: (val) =>
-                        val.isEmpty ? 'Please Enter email address' : null,
-                    onChanged: (val) {
-                      setState(() => email = val);
-                    },
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    'Password',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    validator: (val) => val.length < 8
-                        ? 'Password should contains at least 8 digit'
-                        : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    },
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(primary: Colors.blue),
-                    child: Text(
-                      'Sign In',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        dynamic result = await _auth.signInWithEmailAndPassword(
-                            email, password);
-                        if (result == null) {
-                          setState(() => error = 'Invalid data');
-                        }
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.signInWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() => error = 'Invalid data');
                       }
-                    },
-                  ),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  ),
-                ],
-              ),
+                    }
+                  },
+                ),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
+                ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/Pages/home/home.dart
+++ lib/Pages/home/home.dart
@@ -7,76 +7,80 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
- edit_information_page
-        backgroundColor: Colors.blue[400],
-        appBar: AppBar(
-          title: Text('Membership club'),
-          backgroundColor: Colors.blue[800],
-          elevation: 0.0,
-          actions: <Widget>[
-            TextButton.icon(
-              icon: Icon(
-                Icons.logout,
-                color: Colors.black,
+      backgroundColor: Colors.blue[400],
+      appBar: AppBar(
+        title: Text('Membership club'),
+        backgroundColor: Colors.blue[800],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            icon: Icon(
+              Icons.logout,
+              color: Colors.black,
+            ),
+            label: Text(
+              'Logout',
+              style: TextStyle(color: Colors.black),
+            ),
+            onPressed: () async {
+              await _auth.logOut();
+            },
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
               ),
-              label: Text(
-                'Logout',
-                style: TextStyle(color: Colors.black),
+              child: Text(
+                'Be On Point',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
               ),
-              onPressed: () async {
-                await _auth.logOut();
-              },
-            )
+            ),
+            ListTile(
+              title: Text('Edit Information'),
+              leading: Icon(Icons.account_circle_outlined),
+            ),
+            ListTile(
+              title: Text('Use My Points'),
+              leading: Icon(Icons.add_shopping_cart_outlined),
+            ),
+            ListTile(
+              title: Text('Personal Information'),
+              leading: Icon(Icons.people),
+            ),
+            ListTile(
+              title: Text('Contact Us'),
+              leading: Icon(Icons.email),
+            ),
           ],
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: const <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                ),
-                child: Text(
-                  'Be On Point',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              ListTile(
-                title: Text('Use My Points'),
-                leading: Icon(Icons.money),
-              ),
-              ListTile(
-                title: Text('Personal Information'),
-                leading: Icon(Icons.people),
-              ),
-              ListTile(
-                title: Text('Contact us'),
-                leading: Icon(Icons.email),
-              ),
-            ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        fixedColor: Colors.blueGrey,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.home),
           ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          fixedColor: Colors.blueGrey,
-          items: [
-            BottomNavigationBarItem(
-              title: Text("Home"),
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              title: Text("Search"),
-              icon: Icon(Icons.search),
-            ),
-            BottomNavigationBarItem(
-              title: Text("Profile"),
-              icon: Icon(Icons.account_circle),
-            ),
-            //Action()
-          ],
-        ));
+          BottomNavigationBarItem(
+            title: Text("Search"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Profile"),
+            icon: Icon(Icons.account_circle),
+          ),
+          //Action()
+        ],
+      ),
+    );
   }
 }
